701089b9ed021734f9010197b20cebf0a9b2b3c0
fieldyn <fieldyn@gmail.com>
2018-07-15 23:31:35 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -26,6 +26,16 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((result) => {
+        res.send({
+            todos: result
+        });
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
