014fbb22f55b85f2937ccb750a997f87cad01362
fieldyn <fieldyn@gmail.com>
2018-08-04 11:13:43 -0400

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_MLAB || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_MLAB);
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,7 +7,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 const { ObjectId } = require('mongodb');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const app = express();
 
